5227266ce82158bc4d27e1f1b245dbf74d3baaf3
Lucalel <luca.sichi@hotmail.com>
2021-05-03 14:29:16 +0200

    webserver tests

--- webServer/src/main/java/com/inCounter/test/ManualUserEndTest.java
+++ webServer/src/main/java/com/inCounter/test/ManualUserEndTest.java
@@ -4,15 +4,15 @@ public class ManualUserEndTest {
     public static void main(String args[]){
         UserEndThread userEndThread = new UserEndThread();
         Thread t = new Thread(userEndThread);
-        t.setName("0");
+        t.setName("1");
         t.start();
         UserEndThread userEndThread2 = new UserEndThread();
         Thread t2 = new Thread(userEndThread2);
-        t2.setName("0");
+        t2.setName("2");
         t2.start();
         UserEndThread userEndThread3 = new UserEndThread();
         Thread t3 = new Thread(userEndThread3);
-        t3.setName("0");
+        t3.setName("3");
         t3.start();
         try {
             t.join();
