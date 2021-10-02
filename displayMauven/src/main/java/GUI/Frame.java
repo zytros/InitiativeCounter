3e5ee73d7f6a533f5218fda809b8635501133a68
lucas <luca.sichi@hotmail.com>
2021-10-02 12:51:53 +0200

    mjesetsch

--- displayMauven/src/main/java/GUI/Frame.java
+++ displayMauven/src/main/java/GUI/Frame.java
@@ -43,7 +43,29 @@ public class Frame extends Thread {
         frame.setSize(500,500);
         frame.setVisible(true);
 
+        CharPanel char1 = new CharPanel("DummyOne", 0, 3);
+        CharPanel char2 = new CharPanel("DummyTwo", 0, 2);
+        CharPanel char3 = new CharPanel("DummyThree", 0, 1);
 
+        c.gridx = 0;
+        c.gridy = 1;
+        pane.add(char1, c);
+        c.insets = new Insets(40, 0,0, 0);
+        frame.add(char1, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        pane.add(char2, c);
+        c.insets = new Insets(40, 0,0, 0);
+        frame.add(char2, c);
+
+        c.gridx = 0;
+        c.gridy = 3;
+        pane.add(char3, c);
+        c.insets = new Insets(40, 0,0, 0);
+        frame.add(char3, c);
+
+        //frame.add(panel);
 
         while (true) {
 
@@ -53,6 +75,12 @@ public class Frame extends Thread {
             }
 
             charContainer.getTopX(topX);
+
+            char1 = topX.get(0).getPanel();
+            char2 = topX.get(2).getPanel();
+            char3 = topX.get(1).getPanel();
+
+            /*
 
             for (int i = 0; i < 3; i++) {
                 CharPanel working = topX.remove(0).getPanel();
@@ -69,7 +97,9 @@ public class Frame extends Thread {
                 //panel.repaint();
             }
             frame.add(panel);
-            //try {Thread.sleep(3000);} catch (InterruptedException e) {}
+            */
+
+            try {Thread.sleep(3000);} catch (InterruptedException e) {}
 
         }
 
